4530d83e676794af17bf894a5425bfa3a25bb178
Ben Fox <benjamin.fox@smashingideas.com>
2019-04-19 11:31:06 -0700

    Further simplify TabNavigator

--- navigation/MainTabNavigator.js
+++ navigation/MainTabNavigator.js
@@ -1,23 +1,11 @@
-import { createStackNavigator, createBottomTabNavigator, withNavigation } from 'react-navigation';
+import { createBottomTabNavigator, withNavigation } from 'react-navigation';
 
 import HomeScreen from '../screens/HomeScreen';
 import LinksScreen from '../screens/LinksScreen';
 import SettingsScreen from '../screens/SettingsScreen';
 
-const HomeStack = createStackNavigator({
-  Home: HomeScreen,
-});
-
-const LinksStack = createStackNavigator({
-  Links: LinksScreen,
-});
-
-const SettingsStack = createStackNavigator({
-  Settings: SettingsScreen,
-});
-
 export default withNavigation(createBottomTabNavigator({
-  HomeStack,
-  LinksStack,
-  SettingsStack,
+  HomeScreen,
+  LinksScreen,
+  SettingsScreen,
 }))
